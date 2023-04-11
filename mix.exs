c95b5deef0ca955b6d6590a16b2db51a09e63c14
trbKnl <ncdeschipper@gmail.com>
2023-04-11 21:13:44 +0200

    bumb version

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule Portserver.MixProject do
   defp deps do
     [
       {:bcrypt_elixir, "~> 3.0"},
-      {:phoenix, "~> 1.7.1"},
+      {:phoenix, "~> 1.7.2"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:cloak_ecto, "~> 1.2.0"},
